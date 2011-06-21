185d1bd8f7e8a3393f3ef8dc9b232e189e8a0b02
Floréal TOUMIKIAN <ftoumikian@april.org>
2011-06-21 11:08:58 +0200

    Added directory architecture creation in build script

--- build.sh
+++ build.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
+make_dirs()
+{
+	echo "Creating directories..."
+	mkdir -p pkg
+	mkdir -p src/usr/share/selenium-server
+	mkdir -p src/var/lib/selenium-server
+	mkdir -p src/var/log/selenium-server
+	mkdir -p src/var/run/selenium-server
+}
+
 case $1 in
 	clean)
 		rm -Rf pkg
 		;;
 	*)
-		dpkg-deb --build src > /dev/null
+		make_dirs
+		echo "Downloading selenium-server..."
 		wget http://selenium.googlecode.com/files/selenium-server-standalone-2.0b3.jar -O src/usr/share/selenium-server/selenium-server.jar
-		mkdir -p pkg
+		echo "Building debian package..."
+		dpkg-deb --build src > /dev/null
 		mv src.deb pkg/selenium-server.deb
 		;;
 esac
