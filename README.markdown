a38d5bd40c4cc64a594d1c99b5a9571899d6059e
Floréal TOUMIKIAN <ftoumikian@april.org>
2011-06-24 10:48:19 +0200

    Now package can be built for specific versions

--- README.markdown
+++ README.markdown
@@ -1,8 +1,16 @@
+# Synopsis
+
+This project is meant to automate debian package for selenium-server
+It will automatically download selenium-server from google code
+file repository and package it with init.d scripts. 
+
 # Builing
 
 In order to build the debian package just run
-  $ ./build.sh
-  
+  $ ./build.sh pkg [version]
+
+Then it will be found in pkg sub directory
+ 
 To remove the build package:
   $ ./build.sh clean
   
--- build.sh
+++ build.sh
@@ -1,25 +1,77 @@
 #!/bin/sh
 
-make_dirs()
-{
-	echo "Creating directories..."
-	mkdir -p pkg
-	mkdir -p src/usr/share/selenium-server
-	mkdir -p src/var/lib/selenium-server
-	mkdir -p src/var/log/selenium-server
-	mkdir -p src/var/run/selenium-server
+version=$2
+here=`dirname $0`
+dest_dl_dir=$here/servers
+src_dir=$here/src
+build_dir=$here/build
+pkg_dir=$here/pkg
+
+select_version() {
+	if [ -z $version ] ; then
+		version=2.0rc2
+	fi
+	echo "Builing version $version"
+}
+
+make_dirs() {
+	echo "Creating directories ..."
+	mkdir -p $pkg_dir
+	mkdir -p $build_dir
+	mkdir -p $dest_dl_dir
+	mkdir -p $src_dir/usr/share/selenium-server
+	mkdir -p $src_dir/var/lib/selenium-server
+	mkdir -p $src_dir/var/log/selenium-server
+	mkdir -p $src_dir/var/run/selenium-server
+}
+
+download() {
+	echo "Downloading selenium-server ..."
+	jar=selenium-server-standalone-$version.jar
+	selected_file=$dest_dl_dir/$jar
+	if [ ! -f $selected_file ] ; then
+		wget -nv http://selenium.googlecode.com/files/$jar -O $selected_file || exit 1
+	else
+		echo "  File already downloaded." 
+	fi
+}
+
+build() {
+	echo "Processing ..."
+	rm -Rf $build_dir
+	cp -R $src_dir $build_dir
+	find $build -type f -exec sed -i s/2.0rc3/$version/g {} \;
+	cp $selected_file $build_dir/usr/share/selenium-server/selenium-server.jar
+}
+
+clean() {
+	echo "Cleanning ..."
+	rm -Rf $pkg_dir $build_dir $dest_dl_dir
+}
+
+package() {
+	echo 'Packaging ...'
+	dpkg-deb --build $build_dir > /dev/null
+	mv $here/build.deb $pkg_dir/selenium-server-$version.deb	
+}
+
+usage() {
+	echo "usage:"
+	echo "  `basename $0` pkg [ version ]"
+	echo "  `basename $0` clean"
 }
 
 case $1 in
 	clean)
-		rm -Rf pkg
+		clean
+		;;
+	pkg)
+		select_version
+		make_dirs
+		download
+		build
+		package
 		;;
 	*)
-		make_dirs
-		echo "Downloading selenium-server..."
-		wget http://selenium.googlecode.com/files/selenium-server-standalone-2.0b3.jar -O src/usr/share/selenium-server/selenium-server.jar
-		echo "Building debian package..."
-		dpkg-deb --build src > /dev/null
-		mv src.deb pkg/selenium-server.deb
-		;;
+		usage
 esac
